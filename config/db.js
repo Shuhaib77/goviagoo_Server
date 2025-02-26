bae76115b7ca7c74f500551ae089b9feae1f5d86
Shuhaib77 <shuhaibbasheer20022@gmail.com>
2025-02-26 14:48:16 +0530

    update

--- config/db.js
+++ config/db.js
@@ -1,7 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv'
+
+dotenv.config()
 const connectdb = () => {
+  
   mongoose
-    .connect("mongodb://localhost:27017/Goviagooo")
+    .connect(process.env.MONGO_URI)
     .then(() => {
       console.log("connected");
     })
